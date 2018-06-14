3077f5386cb09752ea4f138d3994712cd570918b
Tom Royal <tom@tomroyal.com>
2018-06-14 13:48:13 +0100

    initial

---
+++ rm_europe.php
@@ -0,0 +1,68 @@
+<?php
+
+// countries in Royal Mail Europe zone
+// https://www.royalmail.com/international-zones/#europe
+// $result = array_search('XX', $rm_europe_countries);
+// returns country name or false
+
+$rm_europe_countries = array
+(
+  'Albania' => 'AL',
+  'Andorra' => 'AD',
+  'Armenia' => 'AM',
+  'Austria' => 'AT',
+  'Azerbaijan' => 'AZ',
+  'Belarus' => 'BY',
+  'Belgium' => 'BE',
+  'Bosnia Herzegovina' => 'BA',
+  'Bulgaria' => 'BG',
+  'Croatia' => 'HR',
+  'Cyprus' => 'CY',
+  'Czech Republic' => 'CZ',
+  'Denmark' => 'DK',
+  'Estonia' => 'EE',
+  'Faroe Islands' => 'FO',
+  'Finland' => 'FI',
+  'France' => 'FR',
+  'Georgia' => 'GE',
+  'Germany' => 'DE',
+  'Gibraltar' => 'GI',
+  'Greece' => 'GR',
+  'Greenland' => 'GL',
+  'Hungary' => 'HU',
+  'Iceland' => 'IS',
+  'Irish Republic' => 'IE',
+  'Italy' => 'IT',
+  'Kazakhstan' => 'KZ',
+  'Kyrgyzstan' => 'KG',
+  'Latvia' => 'LV',
+  'Liechtenstein' => 'LI',
+  'Lithuania' => 'LT',
+  'Luxembourg' => 'LU',
+  'Macedonia' => 'MK',
+  'Malta' => 'MT',
+  'Moldova' => 'MD',
+  'Monaco' => 'MC',
+  'Montenegro' => 'ME',
+  'Netherlands' => 'NL',
+  'Norway' => 'NO',
+  'Poland' => 'PL',
+  'Portugal' => 'PT',
+  'Romania' => 'RO',
+  'Russia' => 'RU',
+  'San Marino' => 'SM',
+  'Serbia' => 'RS',
+  'Slovakia' => 'SK',
+  'Slovenia' => 'SI',
+  'Spain' => 'ES',
+  'Sweden' => 'SE',
+  'Switzerland' => 'CH',
+  'Tajikistan' => 'TJ',
+  'Turkey' => 'TR',
+  'Turkmenistan' => 'TM',
+  'Ukraine' => 'UA',
+  'Uzbekistan' => 'UZ',
+  'Vatican City State' => 'VA'
+);
+
+?>
